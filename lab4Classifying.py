19bdcd99e7272df52ea0666f600cea54321ba7f9
Aleksandr <104778599+peacekeeper228@users.noreply.github.com>
2022-05-25 17:14:39 +0300

    улучшение алгоритма работы
    Изменен механизм сжатия пространства (PCA->UMAP)
    Оценка результата теперь проводится по 4 метрикам

--- lab4Classifying.py
+++ lab4Classifying.py
@@ -1,20 +1,36 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+import joblib
+
 arrX = np.load('lab4/ClassificationX.npy')
 arrY = np.load('lab4/ClassificationY.npy')
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(arrX, arrY,test_size=0.25)
-model = KNeighborsClassifier()
-model.fit(X_train,y_train)
-res=model.predict(X_test)
-def calc_prec(y_hat2, y_test):
-    prec=0
-    for i, j in zip(y_hat2, y_test):
-        if i==j:
-            prec+=1
-    prec/=len(y_test)
-    return prec
+X, X_val, Y, Y_val = train_test_split(arrX, arrY,test_size=0.10)
+maxAccuracy=0
+params={}
 
-print(calc_prec(res, y_test))
+def buildModel():
+    return KNeighborsClassifier(n_neighbors=11)
+
+for _ in range(100):
+    X_train, X_test, y_train, y_test = train_test_split(X, Y,test_size=0.20)
+    model = buildModel()
+    model.fit(X_train,y_train)
+    res=model.predict(X_test)
+    accuracy=f1_score(res, y_test)
+    if accuracy>maxAccuracy:
+        maxAccuracy=accuracy
+        params=model.get_params()
+
+modelRes=buildModel()
+modelRes.set_params(**params)
+res=model.predict(X_test)
+print('Итоговая доля правильных ответов: {:4.2f}%'.format(accuracy_score(res, y_test)*100))
+print('Итоговая точность: {:4.2f}%'.format(precision_score(res, y_test)*100))
+print('Итоговая полнота: {:4.2f}%'.format(recall_score(res, y_test)*100))
+print('Итоговая метрика f1: {:4.2f}%'.format(f1_score(res, y_test)*100))
+
+# Save to file in the current working directory
+joblib_file = "lab4/model.pkl"
+joblib.dump(modelRes, joblib_file)
--- lab4Clustering.py
+++ lab4Clustering.py
@@ -1,29 +1,38 @@
 from sklearn.decomposition import PCA
 import numpy as np
 import matplotlib.pyplot as plt
+import umap
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
+
 w2v_vectors = np.load('lab4/ArticlesVectors.npy')
+#сжатие размерности PCA
 pca2 = PCA(n_components=2)
 pca2.fit(w2v_vectors)
 x_3d = pca2.transform(w2v_vectors)
 x,y = zip(*x_3d)
 plt.scatter(x, y)
 plt.show()
+# сжатие размерности UMAP
+reducer = umap.UMAP()
+embedding = reducer.fit_transform(w2v_vectors)
+plt.scatter(
+    embedding[:, 0],
+    embedding[:, 1])
+plt.show()
 
-
-from sklearn.metrics import silhouette_score
-from sklearn.metrics import adjusted_rand_score
-from sklearn.cluster import KMeans
 score=[]
-for n_clusters in range (2,100):
-    clusterer = KMeans(n_clusters=n_clusters).fit(x_3d)
-    score.append(silhouette_score(x_3d, clusterer.labels_))
-    print("For n_clusters = {}, silhouette score is {}".format(n_clusters, score[n_clusters-2]))
+listCluster = [i for i in range(2,21)]
+for n_clusters in listCluster:
+    clusterer = KMeans(n_clusters=n_clusters).fit(embedding)
+    score.append(silhouette_score(embedding, clusterer.labels_))
+    print("For n clusters = {}, silhouette score is {}".format(n_clusters, score[n_clusters-2]))
 
-plt.plot(score)
+plt.plot(listCluster,score)
+plt.xticks(np.arange(2,21,1))
 plt.show()
 max_value = max(score)
 nClusters = score.index(max_value)+2
-nClusters = 4
-clusterer = KMeans(n_clusters=nClusters).fit(w2v_vectors)
+clusterer = KMeans(n_clusters=nClusters).fit(embedding)
 np.save('lab4/ClassificationX', x_3d)
 np.save('lab4/ClassificationY', clusterer.labels_)
